d4a73840ed3aedbc2ee98a6da50f30987e5a2241
Vrana710 <73100366+Vrana710@users.noreply.github.com>
2025-02-03 15:35:52 +0100

    Added main function to fix runtime error

---
+++ HTTPClients/JSON/JSON_Syntax/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+const issueList = `
+[
+    {
+        "id": 0,
+        "name": "Fix the thing",
+        "estimate": 0.5,
+        "completed": false
+    },
+    {
+        "id": 1,
+        "name": "Unstick the widget",
+        "estimate": 30,
+        "completed": false
+    }
+]
+`
+
+const userObject = `
+{
+    "name": "Wayne Lagner",
+    "role": "Developer",
+    "remote": true
+}
+`
+
+func main() {
+    fmt.Println("JSON Data:")
+    fmt.Println(issueList)
+    fmt.Println(userObject)
+}
